b1f1ee018b8ba4002ee0dc98f64a24f504dd3431
joostf <j.faber@hva.nl>
2026-01-20 13:37:51 +0100

    Setup SvelteKit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Digital Proof Empowerment</h1>
+<p>From Underutilized to Empowered</p>
+
+<p>This project focuses on addressing labor exploitation and digital illiteracy. Using digital technology, we help vulnerable groups become more resilient. We use a multidisciplinary approach combining ethics, technology, and education. In participatory workshops and co-creation sessions with diverse stakeholders — including workers and advocates — we gained valuable insights for impactful solutions. We developed a prototype for a data app that enables workers to record and visualize their working conditions to collect evidence providing proof of labor exploitation and modern slavery.</p>
+
+<p>The findings of the project so far, demonstrate that digital tools and data literacy are essential for improving working conditions and combating exploitation. Inclusive and empathetic approaches are needed to bridge the digital divide.</p>
