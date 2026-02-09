b9821360f885b64d50135cb0296f2939ffc9d55d
Alia Taher <alia.taher022@gmail.com>
2026-02-09 16:29:23 +0100

    refactor: improve Link component structure and styling

--- src/lib/components/atoms/Link.svelte
+++ src/lib/components/atoms/Link.svelte
@@ -1,40 +1,40 @@
 <script>
-
-let {href="#", target="_self ", rel, text, className } = $props();
-
+  let {
+    href = "#",
+    target = "_self ",
+    rel,
+    text,
+    className,
+    ariaLabel,
+  } = $props();
 </script>
 
 <!-- When using this component enter class name as className. -->
 
-<a
-href={href}
-target= {target}
-rel={rel}
-class={className}
->
-{text}
+<a {href} {target} {rel} class="link {className}" aria-label={ariaLabel}>
+  {text}
 </a>
 
 <style>
+  .link:focus {
+    outline: 1px solid var(--accent-neutral);
+    font-size: clamp(14px, 2.5vw, 18px)
+  }
+  .footer-link {
+    /* background-color:var(--primary-darkest) ; */
+    color: black;
+    font-weight: 500;
+    text-decoration: none;
+    margin: 2rem;
+    padding: 0.5rem;
+    letter-spacing: 0.03rem;
+    border: none;
+  }
 
-.footer-link{
-color : white;
-font-weight: 500;
-text-decoration: none;
-margin: 2rem;
-padding: .5rem;
-letter-spacing:  .03rem;
-border: none;
-
-}
-
-.footer-link:visited{
-    color: white;
-
-}
-.footer-link:hover{
+  .footer-link:visited {
+    color: black;
+  }
+  .footer-link:hover {
     font-size: larger;
-}
-
-
+  }
 </style>
