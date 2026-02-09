1a8a1fa0f7ed8b8fce8c59988bb8dc24163fd5bd
Ravirkt <r_tjikhoeri@live.com>
2026-02-09 10:50:37 +0100

    fix: added & to headings in body tag #23

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,11 @@
 <p>This project focuses on addressing labor exploitation and digital illiteracy. Using digital technology, we help vulnerable groups become more resilient. We use a multidisciplinary approach combining ethics, technology, and education. In participatory workshops and co-creation sessions with diverse stakeholders — including workers and advocates — we gained valuable insights for impactful solutions. We developed a prototype for a data app that enables workers to record and visualize their working conditions to collect evidence providing proof of labor exploitation and modern slavery.</p>
 
 <p>The findings of the project so far, demonstrate that digital tools and data literacy are essential for improving working conditions and combating exploitation. Inclusive and empathetic approaches are needed to bridge the digital divide.</p>
+
+
+<h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias, nulla?</h1>
+<h2 class="h1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias, nulla?</h2>
+<h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias, nulla?</h3>
+<h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias, nulla?</h4>
+<h5>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias, nulla?</h5>
+<h6 class="h1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias, nulla?</h6>
