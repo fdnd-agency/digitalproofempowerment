617c55b1f0d029e475eec0d4a7bde2d23f3d52ac
Ravirkt <r_tjikhoeri@live.com>
2026-02-12 12:59:39 +0100

    fix: added ; to exports #6

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,25 +1,25 @@
 // Icons
-export { default as KeyIcon } from "$lib/assets/svg/KeyIcon.svelte"
-export { default as CheckedIcon } from "$lib/assets/svg/CheckedIcon.svelte"
-export { default as LanguageIcon } from "$lib/assets/svg/LanguageIcon.svelte"
-export { default as EyeIcon } from "$lib/assets/svg/EyeIcon.svelte"
-export { default as CloseIcon } from "$lib/assets/svg/CloseIcon.svelte"
-export { default as UploadIcon } from "$lib/assets/svg/UploadIcon.svelte"
-export { default as PhotoUpload } from "$lib/assets/svg/PhotoUpload.svelte"
-export { default as PlayIcon } from "$lib/assets/svg/PlayIcon.svelte"
-export { default as FolderIcon } from "$lib/assets/svg/FolderIcon.svelte"
-export { default as PhoneIcon } from "$lib/assets/svg/PhoneIcon.svelte"
-export { default as VideoIconCard } from "$lib/assets/svg/VideoIconCard.svelte"
-export { default as LocationIconCard } from "$lib/assets/svg/LocationIconCard.svelte"
-export { default as NotesIconCard } from "$lib/assets/svg/NotesIconCard.svelte"
-export { default as AudioIconCard } from "$lib/assets/svg/AudioIconCard.svelte"
-export { default as ChatLogIconCard } from "$lib/assets/svg/ChatLogIconCard.svelte"
+export { default as KeyIcon } from "$lib/assets/svg/KeyIcon.svelte";
+export { default as CheckedIcon } from "$lib/assets/svg/CheckedIcon.svelte";;
+export { default as LanguageIcon } from "$lib/assets/svg/LanguageIcon.svelte";
+export { default as EyeIcon } from "$lib/assets/svg/EyeIcon.svelte";
+export { default as CloseIcon } from "$lib/assets/svg/CloseIcon.svelte";
+export { default as UploadIcon } from "$lib/assets/svg/UploadIcon.svelte";
+export { default as PhotoUpload } from "$lib/assets/svg/PhotoUpload.svelte";
+export { default as PlayIcon } from "$lib/assets/svg/PlayIcon.svelte";
+export { default as FolderIcon } from "$lib/assets/svg/FolderIcon.svelte";
+export { default as PhoneIcon } from "$lib/assets/svg/PhoneIcon.svelte";
+export { default as VideoIconCard } from "$lib/assets/svg/VideoIconCard.svelte";
+export { default as LocationIconCard } from "$lib/assets/svg/LocationIconCard.svelte";
+export { default as NotesIconCard } from "$lib/assets/svg/NotesIconCard.svelte";
+export { default as AudioIconCard } from "$lib/assets/svg/AudioIconCard.svelte";
+export { default as ChatLogIconCard } from "$lib/assets/svg/ChatLogIconCard.svelte";
 
 // Components
-export { default as Button } from "$lib/components/atoms/Button.svelte"
-export { default as Title } from "$lib/components/atoms/Title.svelte"
-export { default as InformationCard} from "$lib/components/molecules/InformationCard.svelte"
-export { default as Nav } from "$lib/components/organisms/Nav.svelte"
-export { default as Link } from "$lib/components/atoms/Link.svelte"
-export { default as Footer } from "$lib/components/organisms/Footer.svelte"
+export { default as Button } from "$lib/components/atoms/Button.svelte";
+export { default as Title } from "$lib/components/atoms/Title.svelte";
+export { default as InformationCard} from "$lib/components/molecules/InformationCard.svelte";
+export { default as Nav } from "$lib/components/organisms/Nav.svelte";
+export { default as Link } from "$lib/components/atoms/Link.svelte";
+export { default as Footer } from "$lib/components/organisms/Footer.svelte";
 
