780d2ac36e04d6f7fcb54a50c4cf9f218a17c511
Alia Taher <alia.taher022@gmail.com>
2026-02-18 09:51:29 +0100

    feat: update icon sizes for CheckedIcon, EyeIcon, and KeyIcon to 20px

--- src/lib/assets/svg/CheckedIcon.svelte
+++ src/lib/assets/svg/CheckedIcon.svelte
@@ -1,21 +1,20 @@
 <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    class="lucide lucide-circle-check-icon lucide-circle-check"
-    ><circle cx="12" cy="12" r="10" /><path d="m9 12 2 2 4-4" /></svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  class="lucide lucide-circle-check-icon lucide-circle-check"
+  ><circle cx="12" cy="12" r="10" /><path d="m9 12 2 2 4-4" /></svg
 >
 
 <style>
-    svg {
-        flex-shrink: 0;
-        height: 15px;
-        width: 15px;
-        display: block;
-    }
+  svg {
+    flex-shrink: 0;
+    height: 20px;
+    width: 20px;
+    display: block;
+  }
 </style>
-
