64348ce913853fb1fa104f32062339570f329e05
Ravirkt <r_tjikhoeri@live.com>
2026-02-19 09:29:20 +0100

    feat: added labels to card component #29

--- src/lib/components/atoms/Button.svelte
+++ src/lib/components/atoms/Button.svelte
@@ -1,49 +1,69 @@
 <script>
-    let { buttonText, Icon, buttonType } = $props();
+    let { buttonText, Icon, buttonType,  } = $props();
 </script>
-
 
 <button class="basic-button {buttonType}">
     {#if Icon}
-        <Icon class="icon"/>
+        <Icon class="icon" />
     {/if}
     {buttonText}
 </button>
 
-
 <style>
-     .basic-button {
+    a {
+        text-decoration: none;
+    }
+
+    .basic-button {
         display: flex;
         align-items: center;
         gap: var(--spacing-xs);
-        border-radius: var(--radius-xs);
+        border-radius: var(--radius-sm);
         font-size: clamp(10px, 3vw, 14px);
         font-weight: 550;
-        padding: var(--spacing-xs);
         -webkit-box-shadow: var(--box-shadow-webkit);
         box-shadow: var(--box-shadow);
         letter-spacing: 0.03rem;
         cursor: pointer;
+        align-self: flex-start;
+
+
     }
 
     .btn-white {
         background-color: var(--main-background-color);
         color: var(--main-text-color);
-        border: var(--border);
+        /* border: var(--border); */
+        padding: var(--spacing-sm);
+        border: none;
+
+        &:hover {
+            background-color: var(--accent-neutral);
+            color: var(--main-background-color);
+        }
     }
 
     .btn-red {
         background-color: var(--emergency-color);
         color: var(--secondary-text-color);
         border: none;
-        padding: var(--spacing-xs);
+        padding: var(--spacing-sm);
+
+        &:hover {
+            background-color: var(--secondary-text-color);
+            color: var(--emergency-color);
+        }
     }
 
     .btn-black {
         background-color: var(--secondary-background-color);
         color: var(--secondary-text-color);
         border: none;
-        padding: var(--spacing-xs);
-    }
+        padding: var(--spacing-sm);
 
+        &:hover {
+            background-color: var(--accent-neutral);
+            color: var(--secondary-text-color);
+        }
+    }
 </style>
---
+++ src/routes/about us/+page.svelte
@@ -0,0 +1,269 @@
+<script>
+    import {
+        Button,
+        PlayIcon,
+        Title,
+        FolderIcon,
+        PhoneIcon,
+        InformationCard,
+        CardIconAudio,
+        CardIconChat,
+        CardIconImg,
+        CardIconVideo,
+        CardIconNotes,
+        CardIconLocation,
+        Profile,
+        Rating,
+        QuotesIcon,
+        EyeIcon,
+        Header,
+        EmergencySection,
+        ReviewCard,
+        HeaderBG,
+    } from "$lib";
+
+    let {  } = $props();
+</script>
+
+<Header
+    pageLabel="About us"
+    HeroHeading="Empowering you to take control safely"
+    HeroParagraph="A safe place to organize your notes and experiences at work."
+/>
+
+<main>
+  
+
+    <section class="evidence-container">
+        <div class="evidence-container-content-wrapper">
+            <Title
+                headingText="What we provide..."
+                className="evidence-container-title"
+            />
+            <p class="evidence-container-paragraph">
+                Our platform provides a safe and secure way to collect and store
+                your evidence, keeping you in control and protected at all
+                times.
+            </p>
+
+            <div class="button-wrapper">
+                <Button
+                    Icon={FolderIcon}
+                    buttonType="btn-black"
+                    buttonText="Start gathering evidence "
+                />
+            </div>
+        </div>
+
+        <div class="evidence-card-container">
+            <InformationCard
+                Icon={CardIconImg}
+                labels={["Jpeg", "Webp"]}
+                cardTitle="Images"
+                cardSubText="Save videos in MP4 format or capture them instantly using your device’s camera. Easily add recordings from your gallery or shoot new ones on the spot to document events clearly and efficiently."
+            />
+            <InformationCard
+                Icon={CardIconVideo}
+                labels={["MP4", "MP3"]}
+                cardTitle="Video"
+                cardSubText="Save videos in MP4 format or capture them instantly using your device’s camera. Easily add recordings from your gallery or shoot new ones on the spot to document events clearly and efficiently."
+            />
+            <InformationCard
+                Icon={CardIconAudio}
+                labels={["Phone", "MP3"]}
+                cardTitle="Audio recordings"
+                cardSubText="Save videos in MP4 format or capture them instantly using your device’s camera. Easily add recordings from your gallery or shoot new ones on the spot to document events clearly and efficiently."
+            />
+            <InformationCard
+                Icon={CardIconNotes}
+                labels={["Pdf", "Notesapp"]}
+                cardTitle="Notes"
+                cardSubText="Save videos in MP4 format or capture them instantly using your device’s camera. Easily add recordings from your gallery or shoot new ones on the spot to document events clearly and efficiently."
+            />
+            <InformationCard
+                Icon={CardIconLocation}
+                labels={["Google Maps", "GPS"]}
+                cardTitle="Location"
+                cardSubText="Save videos in MP4 format or capture them instantly using your device’s camera. Easily add recordings from your gallery or shoot new ones on the spot to document events clearly and efficiently."
+            />
+            <InformationCard
+                Icon={CardIconChat}
+                labels={["Telegram", "Whatsapp"]}
+                cardTitle="Chat logs"
+                cardSubText="Save videos in MP4 format orera. Easily add  or shoot new ones on the spot to document events clearly and efficiently."
+            />
+        </div>
+    </section>
+
+        <EmergencySection />
+
+
+    <section class="reviews-container">
+        <div class="reviews-container-content-wrapper">
+            <Title
+                headingText="Real people. Real results"
+                className="review-container-title"
+            />
+            <p class="reviews-container-paragraph">
+                Honest stories from real people who have experienced unfavorable
+                working conditions in their own jobs—now receiving compensation
+                because they were able to gather all the evidence against their
+                former employer.
+            </p>
+        </div>
+
+        <div class="reviews-card-container">
+            <ReviewCard
+                reviewerName="John D."
+                profileImage="https://i.pravatar.cc/150"
+                reviewerProffesion="Janitor"
+                dateTimeFormat="2026-15-02"
+                reviewDate="15 Feb 2026"
+                reviewText="Being assaulted on the job was terrifying, and I didn’t know
+                    where to turn. This platform allowed me to collect evidence
+                    safely and take action."
+            />
+            <ReviewCard
+                reviewerName="Gabby C."
+                profileImage="https://i.pravatar.cc/150"
+                reviewerProffesion="Mechanic"
+                dateTimeFormat="2026-15-02"
+                reviewDate="15 Feb 2026"
+                reviewText="Honest stories from real people who have experienced unfavorable
+                working conditions in their own jobs—now receiving compensation
+                because they were able to gather all the evidence against their
+                former employer."
+            />
+            <ReviewCard
+                reviewerName="Carlo J."
+                profileImage="https://i.pravatar.cc/150"
+                reviewerProffesion="Manager"
+                dateTimeFormat="2026-15-02"
+                reviewDate="15 Feb 2026"
+                reviewText="I experienced physical abuse at work and felt powerless.
+                    Thanks to this platform, I was able to document everything
+                    and finally protect my rights."
+            />
+        </div>
+    </section>
+</main>
+
+<style>
+    /* evidence container */
+
+    .evidence-container {
+        padding: var(--spacing-3xl) var(--spacing-xl) var(--spacing-3xl);
+        display: flex;
+        flex-direction: column;
+        gap: var(--spacing-lg);
+        /* background-color: var(--accent-light); */
+        color: var(--main-text-color);
+
+        @media (min-width: 800px) {
+            padding: var(--spacing-3xl) var(--spacing-3xl) var(--spacing-3xl) var(--spacing-3xl);
+            display: flex;
+            flex-direction: column;
+            gap: var(--spacing-lg);
+        }
+    }
+
+    .evidence-container-content-wrapper {
+        display: flex;
+        flex-direction: column;
+        gap: var(--spacing-lg);
+
+        @media (min-width: 800px) {
+            text-align: center;
+            display: flex;
+            flex-direction: column;
+            gap: var(--spacing-lg);
+        }
+    }
+
+    .evidence-container-title {
+    }
+
+    .evidence-container-paragraph {
+        margin: 0 auto;
+        max-width: 60rem;
+        text-wrap: pretty;
+    }
+
+    .button-wrapper {
+        @media (min-width: 800px) {
+            display: flex;
+            justify-content: center;
+            margin-top: var(--spacing-md);
+        }
+    }
+
+    .evidence-card-container {
+        padding-top: var(--spacing-xl);
+        display: grid;
+        justify-content: center;
+        grid-template-columns: 1fr;
+        grid-template-rows: auto;
+        gap: var(--spacing-xl);
+
+        @media (min-width: 760px) {
+            grid-template-columns: 1fr 1fr;
+        }
+
+        @media (min-width: 1100px) {
+            grid-template-columns: 1fr 1fr 1fr;
+        }
+    }
+
+    /* ------------ reviews container */
+
+    .reviews-container {
+        padding: var(--spacing-3xl) var(--spacing-xl) var(--spacing-3xl);
+        display: flex;
+        flex-direction: column;
+        gap: var(--spacing-lg);
+        color: var(--main-text-color);
+
+        @media (min-width: 800px) {
+            padding: var(--spacing-3xl) var(--spacing-3xl) var(--spacing-3xl) var(--spacing-3xl);
+            display: flex;
+            flex-direction: column;
+            gap: var(--spacing-lg);
+        }
+    }
+
+    .reviews-container-content-wrapper {
+        display: flex;
+        flex-direction: column;
+        gap: var(--spacing-lg);
+
+        @media (min-width: 800px) {
+            text-align: center;
+            display: flex;
+            flex-direction: column;
+            gap: var(--spacing-lg);
+        }
+    }
+
+    .reviews-card-container {
+        padding-top: var(--spacing-xl);
+        display: grid;
+        justify-content: center;
+        grid-template-columns: 1fr;
+        grid-template-rows: auto;
+        gap: var(--spacing-xl);
+
+        @media (min-width: 760px) {
+            grid-template-columns: 1fr 1fr;
+        }
+
+        @media (min-width: 1100px) {
+            grid-template-columns: 1fr 1fr 1fr;
+        }
+    }
+
+    .reviews-container-paragraph {
+        margin: 0 auto;
+        max-width: 60rem;
+        text-wrap: pretty;
+    }
+</style>
