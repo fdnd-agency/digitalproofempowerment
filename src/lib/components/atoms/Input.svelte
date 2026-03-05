b8e1de1177a5e9d4d7fc80d80e7448fb09be6617
Alia Taher <alia.taher022@gmail.com>
2026-03-05 13:34:42 +0100

    feat: add Input (atom) component for form handling #84

---
+++ src/lib/components/atoms/Input.svelte
@@ -0,0 +1,10 @@
+<script>
+
+let {type, autocomplete="off",name,value, className, ...rest}=$props();
+</script>
+
+<input type={type} autocomplete={autocomplete} name={name} value={value} class={className} {...rest}>
+
+<style>
+    
+</style>
