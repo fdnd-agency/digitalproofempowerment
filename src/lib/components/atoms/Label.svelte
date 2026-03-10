7a5945c932e104ac8edad224a488a84ad300fe50
Alia Taher <alia.taher022@gmail.com>
2026-03-10 12:25:19 +0100

    style: add optional text label styles #83

--- src/lib/components/atoms/Label.svelte
+++ src/lib/components/atoms/Label.svelte
@@ -8,4 +8,19 @@
 </label>
 
 <style>
+.optional-text-label{
+  display: flex;
+  flex-direction: column;
+  margin: var(--spacing-sm);
+}
+
+.email-label{
+  color: var(--secondary-text-color);
+  margin: var(--spacing-md) 0;
+}
+.email-label-verification{
+  color: var(--secondary-text-color);
+  margin: var(--spacing-md) 0;
+
+}
 </style>
