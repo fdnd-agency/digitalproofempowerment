d0b39706772579c491914539af7e51678d09302f
Ravirkt <r_tjikhoeri@live.com>
2026-03-11 19:07:11 +0100

    feat: added post method to directus database endpoint and return succes message #98 #99

--- src/routes/contact/+page.server.js
+++ src/routes/contact/+page.server.js
@@ -14,3 +14,27 @@ export const actions = {
         if (!form.valid) {
             return { form };
         }
+
+        const endpoint = 'https://fdnd-agency.directus.app/items/digital_proof_empowerment_questions';
+        // const endpoint = 'https://69b07208c63dd197febc4bbf.mockapi.io/questions/questions';
+
+
+        const response = await fetch(endpoint, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                // data: {
+                first_name: form.data.firstname,
+                last_name: form.data.lastname,
+                email: form.data.email,
+                message: form.data.message
+            // }
+            })
+        });
+
+        console.log(form.data);
+
+        console.log(message);
+        return message(form,'Message send successfull');
+    }
+};
