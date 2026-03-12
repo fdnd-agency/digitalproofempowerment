6ad0c30df713d17bc97ead4faed985d9cbd135e4
Ravirkt <r_tjikhoeri@live.com>
2026-03-12 11:43:53 +0100

    fix: validation bug fix #106

--- src/lib/components/organisms/ContactForm.svelte
+++ src/lib/components/organisms/ContactForm.svelte
@@ -31,11 +31,11 @@ const { form, errors, enhance, message, submitting, reset } = superForm(data.for
             <Label inputId="last-name" formId="contact-form" className="contact-form-label" text="Last name" />
             <Input name="lastname" type="text" placeholder="Last name" className="contact-form-input" id="last-name" bind:value={$form.lastname} />
 
- {#if $errors.firstname}
+ {#if $errors.lastname}
 
     <span class="error-message">
         <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-octagon-alert-icon lucide-octagon-alert"><path d="M12 16h.01"/><path d="M12 8v4"/><path d="M15.312 2a2 2 0 0 1 1.414.586l4.688 4.688A2 2 0 0 1 22 8.688v6.624a2 2 0 0 1-.586 1.414l-4.688 4.688a2 2 0 0 1-1.414.586H8.688a2 2 0 0 1-1.414-.586l-4.688-4.688A2 2 0 0 1 2 15.312V8.688a2 2 0 0 1 .586-1.414l4.688-4.688A2 2 0 0 1 8.688 2z"/></svg>        
-        {$errors.firstname}
+        {$errors.lastname}
     </span>
 {/if}
 
