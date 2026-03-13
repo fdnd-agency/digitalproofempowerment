dc718f69df1d80d234c23d4fcc7009fd6bb73675
Ravirkt <r_tjikhoeri@live.com>
2026-03-13 15:58:57 +0100

    feat: added page file for help-others and proof tips #6

---
+++ src/routes/help-others/+page.svelte
@@ -0,0 +1,7 @@
+<h1>help other page</h1>
+
+<style>
+    h1 {
+        padding: 10rem;
+    }
+</style>
---
+++ src/routes/proof-tips/+page.svelte
@@ -0,0 +1,7 @@
+<h1>proof tips page</h1>
+
+<style>
+    h1 {
+        padding: 10rem;
+    }
+</style>
