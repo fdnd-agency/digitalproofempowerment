0a6ec71266d0b53a4f57dca981288a41cb971338
Ravirkt <r_tjikhoeri@live.com>
2026-03-22 13:52:51 +0100

    feat: added errormessage component to form and and added error message when faulty post #103 #98

--- src/lib/components/organisms/ContactForm.svelte
+++ src/lib/components/organisms/ContactForm.svelte
@@ -1,12 +1,10 @@
 <script>
-    import { Button, Input, Label, EyeIcon, LoadingState } from "$lib";
-    // import { enhance } from '$app/forms';
+    import { Button, Input, Label, EyeIcon, LoadingState, FormValidationMessages, ErrorMessageIcon } from "$lib";
     import { superForm } from 'sveltekit-superforms';
-
 
     let { data } = $props();
 
-const { form, errors, enhance, message, submitting, reset } = superForm(data.form, {resetForm: true});
+    const { form, errors, enhance, message, submitting, reset } = superForm(data.form, {resetForm: true});
 
 </script>
 
@@ -20,25 +18,17 @@ const { form, errors, enhance, message, submitting, reset } = superForm(data.for
             <Input name="firstname" type="text" placeholder="First name" className="contact-form-input" id="first-name" bind:value={$form.firstname} />
 
             {#if $errors.firstname}
-    <span class="error-message">
-            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-octagon-alert-icon lucide-octagon-alert"><path d="M12 16h.01"/><path d="M12 8v4"/><path d="M15.312 2a2 2 0 0 1 1.414.586l4.688 4.688A2 2 0 0 1 22 8.688v6.624a2 2 0 0 1-.586 1.414l-4.688 4.688a2 2 0 0 1-1.414.586H8.688a2 2 0 0 1-1.414-.586l-4.688-4.688A2 2 0 0 1 2 15.312V8.688a2 2 0 0 1 .586-1.414l4.688-4.688A2 2 0 0 1 8.688 2z"/></svg>        
-
-        {$errors.firstname}</span>
-{/if}
+                <FormValidationMessages ErrorText={$errors.firstname} Icon={ErrorMessageIcon} className="error-message"  />
+            {/if}
         </div>
 
         <div class="input-group">
             <Label inputId="last-name" formId="contact-form" className="contact-form-label" text="Last name" />
             <Input name="lastname" type="text" placeholder="Last name" className="contact-form-input" id="last-name" bind:value={$form.lastname} />
 
- {#if $errors.lastname}
-
-    <span class="error-message">
-        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-octagon-alert-icon lucide-octagon-alert"><path d="M12 16h.01"/><path d="M12 8v4"/><path d="M15.312 2a2 2 0 0 1 1.414.586l4.688 4.688A2 2 0 0 1 22 8.688v6.624a2 2 0 0 1-.586 1.414l-4.688 4.688a2 2 0 0 1-1.414.586H8.688a2 2 0 0 1-1.414-.586l-4.688-4.688A2 2 0 0 1 2 15.312V8.688a2 2 0 0 1 .586-1.414l4.688-4.688A2 2 0 0 1 8.688 2z"/></svg>        
-        {$errors.lastname}
-    </span>
-{/if}
-
+            {#if $errors.lastname}
+                <FormValidationMessages ErrorText={$errors.lastname} Icon={ErrorMessageIcon} className="error-message"  />
+            {/if}
         </div>
 
         <div class="input-group">
@@ -46,11 +36,7 @@ const { form, errors, enhance, message, submitting, reset } = superForm(data.for
             <Input name="email" type="email" placeholder="emailadress" className="contact-form-input" id="email" bind:value={$form.email}  />
             
             {#if $errors.email}
-                <span class="error-message">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-octagon-alert-icon lucide-octagon-alert"><path d="M12 16h.01"/><path d="M12 8v4"/><path d="M15.312 2a2 2 0 0 1 1.414.586l4.688 4.688A2 2 0 0 1 22 8.688v6.624a2 2 0 0 1-.586 1.414l-4.688 4.688a2 2 0 0 1-1.414.586H8.688a2 2 0 0 1-1.414-.586l-4.688-4.688A2 2 0 0 1 2 15.312V8.688a2 2 0 0 1 .586-1.414l4.688-4.688A2 2 0 0 1 8.688 2z"/></svg>        
-                    {$errors.email}
-
-                </span>
+                <FormValidationMessages ErrorText={$errors.email} Icon={ErrorMessageIcon} className="error-message"  />
             {/if}
         </div>
 
@@ -59,43 +45,34 @@ const { form, errors, enhance, message, submitting, reset } = superForm(data.for
             <Input name="message" placeholder="Your message" type="textarea" className="contact-form-input" id="message" bind:value={$form.message}  />
 
             {#if $errors.message }
-                <span class="error-message">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-octagon-alert-icon lucide-octagon-alert"><path d="M12 16h.01"/><path d="M12 8v4"/><path d="M15.312 2a2 2 0 0 1 1.414.586l4.688 4.688A2 2 0 0 1 22 8.688v6.624a2 2 0 0 1-.586 1.414l-4.688 4.688a2 2 0 0 1-1.414.586H8.688a2 2 0 0 1-1.414-.586l-4.688-4.688A2 2 0 0 1 2 15.312V8.688a2 2 0 0 1 .586-1.414l4.688-4.688A2 2 0 0 1 8.688 2z"/></svg>        
-                    {$errors.message}
-                </span>
+                <FormValidationMessages ErrorText={$errors.message} Icon={ErrorMessageIcon} className="error-message"  />
             {/if}
         </div>
     </fieldset>
 
 
-<div class="button-wrapper">
+    <div class="button-wrapper">
 
-   <div class="button-wrapper">
-    {#if $message}
-        <Button
-            Icon={EyeIcon}
-            buttonType="submit"
-            className="btn-green"
-            buttonText="Send!"
-        />
-    {:else}
         <Button
             Icon={EyeIcon}
             buttonType="submit"
             className="btn-black"
-            buttonText="Submit"
+            buttonText="Send"
         />
-    {/if}
-</div>
+            
 
-{#if $submitting}
-    <LoadingState/>    
-{/if}
+        {#if $submitting}
+            <LoadingState/>    
+        {/if}
 
-{#if $message}
-    <p class="success-message">{$message}</p>
-{/if}
-</div>
+        {#if $message === 'Message send successfull'}
+                <FormValidationMessages ErrorText={$message} Icon={ErrorMessageIcon} className="success-message"  />
+        {:else if $message === 'Something went wrong'}
+                <FormValidationMessages ErrorText={$message} Icon={ErrorMessageIcon} className="error-message"  />
+        {/if}
+    </div>
+
+            
 
  
 </form>
