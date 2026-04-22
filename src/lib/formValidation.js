f17d5ee453288a794a589ca39e2f535571de0a4b
Ravirkt <r_tjikhoeri@live.com>
2026-04-22 15:08:39 +0200

    feat: added new scheme for location form #192

--- src/lib/formValidation.js
+++ src/lib/formValidation.js
@@ -1,8 +1,18 @@
-import { z } from 'zod/v3';
+import { z } from "zod/v3";
 
 export const validationItems = z.object({
   firstname: z.string().min(2, "Name must be atleast 2 characters long"),
   lastname: z.string().min(2, "Name must be atleast 2 characters long"),
   email: z.string().email("Not a valid emailadress"),
   message: z.string().min(20, "Message must be atleast 10 characters long"),
+});
+
+export const proofSchema = z.object({
+  date: z.string().min(1, "Date is required"),
+  time: z.string().min(1, "Time is required"),
+  location: z.string().min(1, "Location is required"),
+  street: z.string().optional(),
+  postal: z.string().optional(),
+  city: z.string().optional(),
+  country: z.string().optional(),
 });
