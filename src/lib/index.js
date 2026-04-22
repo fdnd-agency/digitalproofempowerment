1c86e3765f4034fe63696976a882b020ee89b9fe
Ravirkt <r_tjikhoeri@live.com>
2026-04-22 15:18:05 +0200

    fix: added form component to index #192

--- src/lib/index.js
+++ src/lib/index.js
@@ -24,6 +24,7 @@ export { default as NotesUpload } from "$lib/assets/svg/NotesUpload.svelte";
 export { default as VideoUpload } from "$lib/assets/svg/VideoUpload.svelte";
 export { default as ChatLogUpload } from "$lib/assets/svg/ChatLogUpload.svelte";
 export { default as AudioUpload } from "$lib/assets/svg/AudioUpload.svelte";
+export { default as TimeIcon } from "$lib/assets/svg/TimeIcon.svelte";
 
 export { default as CardIconAudio } from "$lib/assets/svg/CardIconAudio.svelte";
 export { default as CardIconChat } from "$lib/assets/svg/CardIconChat.svelte";
@@ -33,7 +34,7 @@ export { default as CardIconNotes } from "$lib/assets/svg/CardIconNotes.svelte";
 export { default as CardIconLocation } from "$lib/assets/svg/CardIconLocation.svelte";
 export { default as StarIcon } from "$lib/assets/svg/StarIcon.svelte";
 export { default as QuotesIcon } from "$lib/assets/svg/QuotesIcon.svelte";
-export { default as Favicon} from "$lib/assets/favicon.svg"
+export { default as Favicon } from "$lib/assets/favicon.svg";
 export { default as ErrorMessageIcon } from "$lib/assets/svg/ErrorMessageIcon.svelte";
 
 // img
@@ -52,13 +53,14 @@ export { default as Profile } from "$lib/components/atoms/Profile.svelte";
 export { default as Header } from "$lib/components/organisms/Header.svelte";
 export { default as EmergencySection } from "$lib/components/molecules/EmergencySection.svelte";
 export { default as ReviewCard } from "$lib/components/molecules/ReviewCard.svelte";
-export { default as QuizBox} from "$lib/components/molecules/QuizBox.svelte"
-export { default as FileInput} from "$lib/components/atoms/FileInput.svelte"
-export { default as Form} from "$lib/components/molecules/Form.svelte"
-export { default as Text} from "$lib/components/atoms/Text.svelte"
-export { default as ContactForm} from "$lib/components/organisms/ContactForm.svelte"
-export { default as Label} from "$lib/components/atoms/Label.svelte"
-export { default as Input} from "$lib/components/atoms/Input.svelte"
-export { default as LocationSection} from "$lib/components/organisms/LocationSection.svelte"
-export { default as LoadingState} from "$lib/components/atoms/LoadingState.svelte"
-export { default as FormValidationMessages} from "$lib/components/atoms/FormValidationMessages.svelte"
+export { default as QuizBox } from "$lib/components/molecules/QuizBox.svelte";
+export { default as FileInput } from "$lib/components/atoms/FileInput.svelte";
+export { default as Form } from "$lib/components/molecules/Form.svelte";
+export { default as Text } from "$lib/components/atoms/Text.svelte";
+export { default as ContactForm } from "$lib/components/organisms/ContactForm.svelte";
+export { default as Label } from "$lib/components/atoms/Label.svelte";
+export { default as Input } from "$lib/components/atoms/Input.svelte";
+export { default as LocationSection } from "$lib/components/organisms/LocationSection.svelte";
+export { default as LoadingState } from "$lib/components/atoms/LoadingState.svelte";
+export { default as FormValidationMessages } from "$lib/components/atoms/FormValidationMessages.svelte";
+export { default as SubmitForm } from "$lib/components/organisms/SubmitForm.svelte";
