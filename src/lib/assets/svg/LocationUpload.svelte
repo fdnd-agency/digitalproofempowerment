9ea04f7379d1640253235bbc11e16d40940f8208
Ravirkt <r_tjikhoeri@live.com>
2026-04-22 15:18:36 +0200

    feat: added new icons for form #192

--- src/lib/assets/svg/LocationUpload.svelte
+++ src/lib/assets/svg/LocationUpload.svelte
@@ -1,22 +1,23 @@
 <svg
-  xmlns="http://www.w3.org/2000/svg"
   viewBox="0 0 24 24"
   fill="none"
   stroke="currentColor"
   stroke-width="2"
   stroke-linecap="round"
   stroke-linejoin="round"
-  class="lucide lucide-map-pin-icon lucide-map-pin"
-  ><path
-    d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"
-  /><circle cx="12" cy="10" r="3" /></svg
+  class="lucide lucide-calendar-days-icon lucide-calendar-days"
+  ><path d="M8 2v4" /><path d="M16 2v4" /><rect width="18" height="18" x="3" y="4" rx="2" /><path
+    d="M3 10h18"
+  /><path d="M8 14h.01" /><path d="M12 14h.01" /><path d="M16 14h.01" /><path d="M8 18h.01" /><path
+    d="M12 18h.01"
+  /><path d="M16 18h.01" /></svg
 >
 
 <style>
   svg {
     flex-shrink: 0;
-    height: 15px;
-    width: 15px;
+    height: 20px;
+    width: 20px;
     display: block;
   }
 </style>
---
+++ src/lib/assets/svg/TimeIcon.svelte
@@ -0,0 +1,23 @@
+<svg
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  class="lucide lucide-timer-icon lucide-timer"
+  ><line x1="10" x2="14" y1="2" y2="2" /><line x1="12" x2="15" y1="14" y2="11" /><circle
+    cx="12"
+    cy="14"
+    r="8"
+  /></svg
+>
+
+<style>
+  svg {
+    flex-shrink: 0;
+    height: 20px;
+    width: 20px;
+    display: block;
+  }
+</style>
