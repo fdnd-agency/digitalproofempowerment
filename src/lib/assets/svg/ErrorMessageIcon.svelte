bfdfcab7c1427720fd3dc0c646bf862e11db1ea9
Ravirkt <r_tjikhoeri@live.com>
2026-05-12 19:10:15 +0200

    feat: added new icons for cleaner validation message #191 #192

--- src/lib/assets/svg/ErrorMessageIcon.svelte
+++ src/lib/assets/svg/ErrorMessageIcon.svelte
@@ -1,7 +1,4 @@
 <svg
-  xmlns="http://www.w3.org/2000/svg"
-  width="15"
-  height="15"
   viewBox="0 0 24 24"
   fill="none"
   stroke="currentColor"
@@ -13,3 +10,13 @@
     d="M15.312 2a2 2 0 0 1 1.414.586l4.688 4.688A2 2 0 0 1 22 8.688v6.624a2 2 0 0 1-.586 1.414l-4.688 4.688a2 2 0 0 1-1.414.586H8.688a2 2 0 0 1-1.414-.586l-4.688-4.688A2 2 0 0 1 2 15.312V8.688a2 2 0 0 1 .586-1.414l4.688-4.688A2 2 0 0 1 8.688 2z"
   /></svg
 >
+
+<style>
+  svg {
+    flex-shrink: 0;
+    height: 40px;
+    width: 40px;
+    display: block;
+    stroke: var(--emergency);
+  }
+</style>
