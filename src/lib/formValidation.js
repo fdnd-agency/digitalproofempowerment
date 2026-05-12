febe114286ed4836436a09b0df30f3e7e475448d
Ravirkt <r_tjikhoeri@live.com>
2026-05-12 19:12:03 +0200

    feat: added scheme for validation rules  #191 #192

--- src/lib/formValidation.js
+++ src/lib/formValidation.js
@@ -6,3 +6,11 @@ export const validationItems = z.object({
   email: z.string().email("Not a valid emailadress"),
   message: z.string().min(20, "Message must be atleast 10 characters long"),
 });
+
+export const proofSchema = z.object({
+  date: z.string().min(1, "Date is required"),
+  time: z.string().min(1, "Time is required"),
+  location: z.string().min(1, "Location is required"),
+  houseNumber: z.string().optional(),
+  postalCode: z.string().optional(),
+});
